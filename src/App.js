0971c6d6fbce1c60b177bb3565dd123bdda0fddb
lang-sky <skylang121@outlook.com>
2020-01-29 04:23:22 +0800

    'step-1'

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <Link to="/">Feed</Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+);
+
+const Template = ({ title }) => (
+  <div>
+    <NavBar />
+    <p className="page-info">This is the {title} page</p>
+  </div>
+);
+
+const Feed = () => <Template title="Feed" />;
+
+const Profile = () => <Template title="Profile" />;
+
+const App = () => (
+  <Router>
+    <Route exact path="/" component={Feed} />
+    <Route path="/profile" component={Profile} />
+  </Router>
+);
+
+export default App;
